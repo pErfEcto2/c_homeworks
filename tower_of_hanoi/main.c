737751516982da6bd9a2d27b9f6cdcb5fd4cdc3f
pErfEcto2 <misha.yaro2004@gmail.com>
2023-05-17 10:55:50 +0300

    minor changes

--- tower_of_hanoi/main.c
+++ tower_of_hanoi/main.c
@@ -1,33 +1,36 @@
+/*
+ * Это реализация Андрея, его благодарите
+ */
+
 #include "lib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+  node *head = NULL, *head2 = NULL, *head3 = NULL;
+  pointer *addresses = NULL;
+  int r;
 
-	node *head = NULL,*head2 = NULL, *head3 = NULL;
-	pointer *addresses = NULL;
-	int r;
+  srand(time(NULL));
 
-	srand(time(NULL));
-
-	printf("Generated linked list (method of head)\n");
-	/*while ((r=(rand()%T)) < T/2){
-		if (r==0) r+=1;
-		head = add_head(head, r);
-	}*/
-    for (r=1; r <= T/2; r++){
-        head = add_head(head, r);
-    }
-	pr(head);
-	printf("Length of linked list is: %d\n", length(head));
-	printf("Sorted linked list\n");
-	sort(head);
-	pr(head);
-	addresses = add_head_address(addresses, &head3);
-	addresses = add_head_address(addresses, &head2);
-	addresses = add_head_address(addresses, &head);
-	hanoi_tower(length(head), &head, &head3, &head2, addresses);
-	return 0;
+  printf("Generated linked list (method of head)\n");
+  /*while ((r=(rand()%T)) < T/2){
+          if (r==0) r+=1;
+          head = add_head(head, r);
+  }*/
+  for (r = 1; r <= T / 2; r++) {
+    head = add_head(head, r);
+  }
+  pr(head);
+  printf("Length of linked list is: %d\n", length(head));
+  printf("Sorted linked list\n");
+  sort(head);
+  pr(head);
+  addresses = add_head_address(addresses, &head3);
+  addresses = add_head_address(addresses, &head2);
+  addresses = add_head_address(addresses, &head);
+  hanoi_tower(length(head), &head, &head3, &head2, addresses);
+  return 0;
 }
